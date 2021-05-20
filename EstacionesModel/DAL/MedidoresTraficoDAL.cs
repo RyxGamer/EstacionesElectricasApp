2abc7af1011b31ba903180b91d0bcd78d87824ee
ryxgamer <goku3303@gmail.com>
2021-05-20 09:28:42 -0400

    Agregar archivos de proyecto.

---
+++ EstacionesModel/DAL/MedidoresTraficoDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstacionesModel.DTO;
+
+namespace EstacionesModel.DAL
+{
+    public class MedidoresTraficoDAL : IMedidoresDAL
+    {
+       
+        private MedidoresTraficoDAL()
+        {
+
+        }
+     
+        private static IMedidoresDAL instancia;
+     
+        public static IMedidoresDAL GetInstance()
+        {
+            if (instancia == null)
+                instancia = new MedidoresTraficoDAL();
+            return instancia;
+        }
+
+
+        public List<Medidor> ObtenerMedidores()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
